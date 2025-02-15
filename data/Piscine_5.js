33b74e58e840864e2676e84913bc03752bca3712
jmb974 <jmb974@gmail.com>
2025-02-15 10:30:44 +0100

    Add files via upload

---
+++ data/Piscine_5.js
@@ -0,0 +1,1 @@
+var json_Piscine_5 = {"type":"FeatureCollection","name":"Piscine_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null},"geometry":{"type":"Point","coordinates":[-0.872799858686887,45.827445504415898]}}]}
